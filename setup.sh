fc642775f3af8de3c1c150fdc849eb4041dcfe38
Reid Beels <mail@reidbeels.com>
2013-05-17 01:29:51 -0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#! /bin/sh -e
+
+SCRIPT=$(readlink -f $0)
+BASE=$(dirname $SCRIPT)
+TEMP=$(mktemp -d)
+WORK="$BASE/work"
+
+wget https://api.github.com/repos/reidab/chromebook-setup/tarball -O $TEMP/reidab-chromebook-setup.tar.gz
+wget http://goo.gl/fd3zc -O $TEMP/crouton
+sh -e "$TEMP/crouton" -x "$WORK/crouton-src"
+tar xzf "$TEMP/reidab-chromebook-setup.tar.gz" -C $WORK --strip-components 1
+
+ls $WORK
+
+trap 'rm -rf "$TEMP" >/dev/null 2>&1' 0
+trap "exit 2" 1 2 3 15
